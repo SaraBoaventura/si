5a5d7384cf779f97b1d8469f2549b8f7a50a4d83
SaraBoaventura <pg44381@alunos.uminho.pt>
2021-11-15 18:58:54 +0000

    commit

--- src/si/supervised/model.py
+++ src/si/supervised/model.py
@@ -1,23 +1,19 @@
 from abc import ABC, abstractmethod
 
-class Model(ABC):
 
+class Model(ABC):
     def __init__(self):
-        """
-        Abstract class defining an interface for 
-        supervised learning models
-        """
+        '''Abstract class defining an interface for supervised learning models'''
         self.is_fitted = False
 
     @abstractmethod
-    def fit(self,dataset):
+    def fit(self, dataset):
         raise NotImplementedError
-
+	
     @abstractmethod
-    def predict(self,x):
+    def predict(self, x):
         raise NotImplementedError
-
-    @abstractmethod
+	
     def cost(self):
         raise NotImplementedError
 
